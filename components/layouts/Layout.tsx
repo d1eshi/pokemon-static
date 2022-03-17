893ba85dc2435931441462a22f185a8b56ebc587
d1eshi <silva.diegof03@gmail.com>
2022-03-16 21:18:43 -0300

    [add] meta tags for seo

--- components/layouts/Layout.tsx
+++ components/layouts/Layout.tsx
@@ -6,6 +6,8 @@ interface Props {
   title?: string
 }
 
+const origin = typeof window === 'undefined' ? '' : window.location.origin
+
 export const Layout: FC<Props> = ({ children, title }) => {
   return (
     <>
@@ -14,6 +16,13 @@ export const Layout: FC<Props> = ({ children, title }) => {
         <meta name="author" content="Diego Silva (d1eshi)" />
         <meta name="description" content={`Información del pokemon ${title}`} />
         <meta name="keywords" content={`${title}, pokemon, pokedex`} />
+
+        <meta property="og:title" content={`Información sobre ${title}`} />
+        <meta
+          property="og:description"
+          content={`Esta es la página sobre ${title}`}
+        />
+        <meta property="og:image" content={`${origin}/img/banner.png`} />
       </Head>
 
       <Navbar />
